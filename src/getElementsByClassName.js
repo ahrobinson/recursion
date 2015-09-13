fd86e2d1799bf2e9b75dcc7ecbfbcc32d2ecbfb2
ahrobinson <ahrobins3@gmail.com>
2015-09-12 21:56:07 -0500

    complete getElementsByClassName and stringifyJSON

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,29 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
+var getElementsByClassName = function(className){
   // your code here
+    var matches = [];
+    var doc = document;
+    function recursive(docNodes){
+      /*traverse element childNodes. Non-elements have classList, but is undefined
+      so you must check for that or you will get a TypeError
+      */
+      for(var i = 0; i < docNodes.childNodes.length; i++){
+        if(docNodes.childNodes[i].classList !== undefined && docNodes.childNodes[i].classList.contains(className)){
+          matches.push(docNodes.childNodes[i]);
+        }
+        /*If childNodes have childNodes and they are not undefined, traverse those.
+        Note: childNodes accesses both elements and non-elements. The non-elements
+        have nodes as well but they are undefined so you must check for that.
+        */
+        if(docNodes.childNodes[i].childNodes[0] !== undefined){
+          recursive(docNodes.childNodes[i]);
+        }
+      }
+    }
+    //when you pass in a className, it starts the search with the document
+    recursive(doc);
+    console.log(matches);
+    return matches;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,55 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
+  function isComplex(obj){
+      return typeof obj !== null && typeof obj === "object";
+  }
   // your code goes here
+  if(typeof obj === 'string'){
+    return '"' + obj + '"';
+  } else if(typeof obj === 'number'){
+    return  obj + ""
+  } else if(typeof obj === 'boolean'){
+    return obj.toString();
+  } else if(obj === null){
+    return "null";
+  } else if(typeof obj === 'function'){
+    return;
+  } else if(typeof obj === 'undefined'){
+    return;
+  }
+  var JSONHolder = [];
+  if(Array.isArray(obj)){
+    if(obj.length){
+      for(var i = 0; i < obj.length; i++){
+        if(Array.isArray(obj[i])){
+          JSONHolder.push(stringifyJSON(obj[i]));
+        } else {
+          JSONHolder.push(stringifyJSON(obj[i]));
+        }
+      }
+    } else {
+      return "[]";
+  }
+    return "[" + JSONHolder + "]";
+
+  } else {
+    if(Object.keys(obj).length){
+      var output = '';
+      for(var prop in obj){
+        if(typeof obj[prop] === 'function' || typeof obj[prop] === 'undefined'){
+          output += '{}';
+          return output;
+        } else if(isComplex(obj[prop])){
+            stringifyJSON(obj[prop]);
+        }
+        output += '"' + prop + '":' + stringifyJSON(obj[prop]) +  ',';
+
+  }
+      return '{' + output.slice(0,-1) + '}';
+    } else {
+      return '{}';
+    }
+
+  }
 };
